a087779dc511a4f1a3fd045d9af0356940f10fbd
medshk <47352636+medshk@users.noreply.github.com>
2022-03-28 15:31:53 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'home': "linear-gradient(rgba(0,0,0,0.2),rgba(0,0,0,0.8))"
+      }
+    },
+  },
+  plugins: [],
+}
